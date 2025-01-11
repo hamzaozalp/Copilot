7ed4334b767cd16705a45968fb9738716d4fe2dc
hamzaozalp <hamzaozalp@yaani.com>
2025-01-11 23:20:45 +0300

    Initial commit

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunmedyaAkademiPortfolio.Models;
+
+namespace AcunmedyaAkademiPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        DbAcunMedyaAkademi1Entities db = new DbAcunMedyaAkademi1Entities();
+
+        public PartialViewResult PartialExperience()
+        {
+            var values = db.TblExperience.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.TblSkıll.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHeroSection()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunmedyaAkademiPortfolio.Models;
+
+namespace AcunmedyaAkademiPortfolio.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        DbAcunMedyaAkademi1Entities db = new DbAcunMedyaAkademi1Entities();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
